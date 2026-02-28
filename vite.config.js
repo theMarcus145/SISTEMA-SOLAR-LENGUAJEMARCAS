4af158a8a9b0ed229d417712fbb8f2defd4d403d
Marco <marcogilvalero@gmail.com>
2026-02-28 13:29:32 +0100

    cambios vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        contacto: resolve(__dirname, "contacto.html"),
+        galeria: resolve(__dirname, "galeria.html"),
+        sol: resolve(__dirname, "sol.html"),
+      },
+    },
   },
 });
