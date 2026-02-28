101add61ce01f474bb5e31b11e6e2fe564a53908
Marco <marcogilvalero@gmail.com>
2026-02-28 13:17:29 +0100

    t

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://unpkg.com/three@0.183.1/build/three.module.js";
+import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 //CREAR ESCENA
